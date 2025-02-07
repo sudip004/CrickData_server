521f2cc5c6f8010125376580dd7513e6d38a6d46
Sudip Basak <84569249+sudip004@users.noreply.github.com>
2025-02-07 16:10:42 +0530

    Update AuthUser.js

--- middlewares/AuthUser.js
+++ middlewares/AuthUser.js
@@ -3,11 +3,12 @@ const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
      const token = req.cookies?.token;
-    // console.log("authuser",token);
+   console.log("auth token",token);
     if (!token) return res.status(401).send('Access denied. No token provided.');
     try {
         const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
         req.user = decoded;
+          console.log("req,user",req.user);
         next();
     } catch (error) {
         console.log("authuserammmmiiii",error);
